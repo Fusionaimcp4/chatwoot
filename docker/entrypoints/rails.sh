aadde92be4393f14ca5b92f97473cf7a8f8a3d2e
Fusionaimcp4 <yilakb@outlook.com>
2025-12-10 13:42:18 -0600

    Fix entrypoint helper script execution and make scripts executable

--- docker/entrypoints/rails.sh
+++ docker/entrypoints/rails.sh
@@ -9,7 +9,9 @@ echo "Waiting for postgres to become ready...."
 
 # Let DATABASE_URL env take presedence over individual connection params.
 # This is done to avoid printing the DATABASE_URL in the logs
-$(docker/entrypoints/helpers/pg_database_url.rb)
+if [ -f /app/docker/entrypoints/helpers/pg_database_url.rb ]; then
+  eval $(/app/docker/entrypoints/helpers/pg_database_url.rb)
+fi
 PG_READY="pg_isready -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USERNAME"
 
 until $PG_READY
