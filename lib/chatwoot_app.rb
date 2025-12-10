0843112389b3cd5458388ff50e17b3f389cca957
Fusionaimcp4 <yilakb@outlook.com>
2025-12-10 10:20:10 -0600

    Create lean Docker build excluding enterprise code and .md files

--- lib/chatwoot_app.rb
+++ lib/chatwoot_app.rb
@@ -12,9 +12,8 @@ module ChatwootApp
   end
 
   def self.enterprise?
-    return if ENV.fetch('DISABLE_ENTERPRISE', false)
-
-    @enterprise ||= root.join('enterprise').exist?
+    # Enterprise disabled for lean self-hosted build
+    false
   end
 
   def self.chatwoot_cloud?
